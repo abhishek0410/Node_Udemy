a4cf8de4b634cd882ff03f800d3f67d131f2398e
Abhishek <u6411540@anu.edu.au>
2019-08-05 13:47:55 +1000

    Lecture successfully implemented

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,18 @@
+const chalk = require("chalk");
 const getNotes = require("./notes");
 
-console.log(getNotes());
+// const msg = getNotes();
+// console.log(msg);
+
+// const greenMsg = chalk.blue.inverse.bold("Success");
+
+// console.log(greenMsg);
+// console.log(process.argv[2]);
+
+const command = process.argv[2];
+
+if (command === "add") {
+  console.log("Adding note");
+} else if (command === "remove") {
+  console.log("Removing Note ");
+}
