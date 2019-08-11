ecb69930966cd54013b6e6f4a5ad8482a105442d
Abhishek <u6411540@anu.edu.au>
2019-08-12 01:00:44 +1000

    Section8_54 completed

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -61,12 +61,30 @@ app.get("/", (req, res) => {
 //   res.send("<h1>Everything About the page here</h1>");
 // });
 
-// app.get("/weather", (req, res) => {
-//   res.send({
-//     forecast: "It is snowing",
-//     location: "Philadelphia"
-//   });
-// });
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address"
+    });
+  }
+  res.send({
+    forecast: "It is snowing",
+    location: "Philadelphia",
+    address: req.query.address
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term"
+    });
+  }
+  console.log(req.query);
+  res.send({
+    products: []
+  });
+});
 
 app.get("/help/*", (req, res) => {
   res.render("404", {
