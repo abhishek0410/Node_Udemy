669c99dc8681e65dbc1f0b96efcfdf2568cd5884
Abhishek <u6411540@anu.edu.au>
2019-08-12 01:40:08 +1000

    section8_55 completed

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 const app = express();
 
@@ -67,10 +69,22 @@ app.get("/weather", (req, res) => {
       error: "You must provide an address"
     });
   }
-  res.send({
-    forecast: "It is snowing",
-    location: "Philadelphia",
-    address: req.query.address
+  // address: req.query.address
+  geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return res.send({ error: "There has been error in fetching the data" });
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error: "There has been error in fetching the data" });
+      }
+      res.send({
+        forecast: forecastData,
+        location: location,
+        address: req.query.address
+      });
+    });
   });
 });
 
