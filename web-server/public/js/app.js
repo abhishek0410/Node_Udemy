6930bf496342db6595468156d78b1fc3b9ffea62
Abhishek <u6411540@anu.edu.au>
2019-08-14 01:14:24 +1000

    section8_58 completed

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,25 +1,21 @@
 console.log("Client side java script is loaded ");
 
-fetch("http://puzzle.mead.io/puzzle").then(response => {
-  response.json().then(data => {
-    console.log(data);
-  });
-});
-
-fetch("http://localhost:8000/weather?address=toronto").then(response => {
-  response.json().then(data => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
-  });
-});
-
 const weathreForm = document.querySelector("form");
+const search = document.querySelector("input");
 
 weathreForm.addEventListener("submit", e => {
   e.preventDefault();
-  console.log("testing");
+
+  const location = search.value;
+
+  fetch("http://localhost:8000/weather?address=" + location).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
+  });
 });
