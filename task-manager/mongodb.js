35cb51a35bf48ad4d0a7574d0aeea86d9fab626d
Abhishek <u6411540@anu.edu.au>
2019-08-14 16:09:22 +1000

    section10_76  completed

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -12,9 +12,59 @@ MongoClient.connect(
       return console.log("Unable to connect to the database");
     }
     const db = client.db(databaseName);
-    db.collection("users").insertOne({
-      name: "Abhi",
-      age: 27
-    });
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Abhi",
+    //     age: 27
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert user ");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Jen",
+    //       age: 28
+    //     },
+    //     {
+    //       name: "Gunther",
+    //       age: 27
+    //     }
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert Document");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    //Creating a task collection in the task-manager database
+    db.collection("task").insertMany(
+      [
+        {
+          description: "Clean your desk ",
+          completed: false
+        },
+        {
+          description: "Greatness lies within",
+          completed: true
+        },
+        {
+          description: "By gones are bygones",
+          completed: true
+        }
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("The data could");
+        }
+        console.log(result.ops);
+      }
+    );
   }
 );
