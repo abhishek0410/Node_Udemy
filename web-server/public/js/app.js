89e6087112e8056b82a94575f713e0a5b1f18c37
Abhishek <u6411540@anu.edu.au>
2019-08-14 15:30:14 +1000

    section10_56 completed

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,19 +2,27 @@ console.log("Client side java script is loaded ");
 
 const weathreForm = document.querySelector("form");
 const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+
+const messageTwo = document.querySelector("#message-2");
+
+// messageOne.textContent = "From Java Scipt";
 
 weathreForm.addEventListener("submit", e => {
   e.preventDefault();
 
   const location = search.value;
 
+  messageOne.textContent = "Loading....";
+  n = messageTwo.textContent = "";
+
   fetch("http://localhost:8000/weather?address=" + location).then(response => {
     response.json().then(data => {
       if (data.error) {
-        console.log(data.error);
+        messageOne.textContent = data.error;
       } else {
-        console.log(data.location);
-        console.log(data.forecast);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
       }
     });
   });
