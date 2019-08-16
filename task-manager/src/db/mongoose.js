c113e98dbe7d215911aa68debea552f2595e5330
Abhishek <u6411540@anu.edu.au>
2019-08-16 14:26:58 +1000

    sectio10_85

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,50 +1,70 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true
 });
 
-// const User = mongoose.model("User", {
-//   name: {
-//     type: String
-//   },
-//   age: {
-//     type: Number
-//   }
-// });
-
-// const me = new User({
-//   name: "Andrew",
-//   age: 27
-// });
-
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch(error => {
-//     console.log("Error !", error);
-//   });
-
-const Task = mongoose.model("task", {
-  description: {
-    type: String
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true
   },
-  completed: {
-    type: Boolean
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is required");
+      }
+    }
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    }
   }
 });
 
-const task1 = new Task({
-  description: "What is the edge of universe",
-  completed: true
+const me = new User({
+  name: "delhi",
+  email: "mike@mead.io"
 });
-task1
-  .save()
-  .then(result => {
-    console.log(result);
+
+me.save()
+  .then(() => {
+    console.log(me);
   })
   .catch(error => {
-    console.log("Error is ", error);
+    console.log("Error !", error);
   });
+
+// const Task = mongoose.model("task", {
+//   description: {
+//     type: String
+//   },
+//   completed: {
+//     type: Boolean
+//   }
+// });
+
+// const task1 = new Task({
+//   description: "What is the edge of universe",
+//   completed: true
+// });
+// task1
+//   .save()
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(error => {
+//     console.log("Error is ", error);
+//   });
